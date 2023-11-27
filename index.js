e604e2153b48a4159e91bf33692ad2a1ff13de7d
Morgane Debarge <morganedebarge.05@gmail.com>
2023-11-27 14:19:17 +0100

    ok

--- index.js
+++ index.js
@@ -1,58 +1,6 @@
-const express = require("express");
-
-const app = express();
+const app = require('./app');
 
 const port = 5000;
-
-const movies = [
-  {
-    id: 1,
-    title: "Citizen Kane",
-    director: "Orson Wells",
-    year: "1941",
-    color: false,
-    duration: 120,
-  },
-  {
-    id: 2,
-    title: "The Godfather",
-    director: "Francis Ford Coppola",
-    year: "1972",
-    color: true,
-    duration: 180,
-  },
-  {
-    id: 3,
-    title: "Pulp Fiction",
-    director: "Quentin Tarantino",
-    year: "1994",
-    color: true,
-    duration: 180,
-  },
-];
-const welcome = (req, res) => {
-    res.send("Welcome to my favourite movie list");
-  };
-  
-  app.get("/", welcome);
-
-  const getApiMovies = (req,res)=>{
-    res.status(200).json(movies)
-  }
-  app.get("/api/movies",getApiMovies);
-
-const getApiMovieId=(req,res)=>{
-    const wantedId = parseInt(req.params.id)
-    const movie = movies.find((movie)=>movie.id===wantedId);
-    
-    if (movie!=null){
-        res.status(200).json(movie)
-    }
-    else{
-        res.status(404).send("Cannot find ");
-    }
-}
-  app.get("/api/movies/:id",getApiMovieId);
 
 app
   .listen(port, () => {
@@ -61,3 +9,5 @@ app
   .on("error", (err) => {
     console.error("Error:", err.message);
   });
+
+
